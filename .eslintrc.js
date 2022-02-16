d1dd610697a8df9b7fdf695fac8fecb7e0920b1d
take-tech1001 <tk.tech1001@gmail.com>
2022-02-16 17:53:21 +0900

    ➕ Add eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: { project: './tsconfig.json' },
+  env: { es2021: true, browser: true },
+  plugins: ['import', 'simple-import-sort', 'tailwindcss', 'import-access'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:tailwindcss/recommended',
+    'prettier'
+  ],
+  rules: {
+    'no-console': ['warn', { allow: ['warn', 'info', 'error'] }],
+    'prefer-arrow-callback': 'error',
+    'prefer-const': 'error',
+    'func-style': ['error', 'expression'],
+
+    'import/newline-after-import': ['error'],
+    'import/no-default-export': 'error',
+    'import-access/jsdoc': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }
+    ]
+  }
+}
