c9a23f7fa186a92b6b7566c8a8332d7ee09da15b
take-tech1001 <tk.tech1001@gmail.com>
2022-03-08 10:15:29 +0900

    ✨ Add option

--- src/options.ts
+++ src/options.ts
@@ -16,21 +16,43 @@ const appendSavedElement = (): void => {
 formButton.onclick = () => {
   const token = <HTMLInputElement>document.getElementById('token')
   const channelID = <HTMLInputElement>document.getElementById('channelID')
+  const timesChannelID = <HTMLInputElement>(
+    document.getElementById('timesChannelID')
+  )
   const myName = <HTMLInputElement>document.getElementById('myName')
+  const toggl = <HTMLInputElement>document.getElementById('toggl')
 
-  if (!token.value && !channelID.value && !myName.value) return
+  if (
+    !token.value &&
+    !channelID.value &&
+    !timesChannelID.value &&
+    !myName.value &&
+    !toggl.value
+  ) {
+    alert('値を入力してください')
+    return
+  }
 
-  chrome.storage.sync.get(['token', 'channelID', 'myName'], (items) => {
-    console.log('token:' + items.token)
-    console.log('channelID:' + items.channelID)
-    console.log('myName:' + items.myName)
+  chrome.storage.sync.get(
+    ['token', 'channelID', 'timesChannelID', 'myName', 'toggl'],
+    (items) => {
+      console.log('token:' + items.token)
+      console.log('channelID:' + items.channelID)
+      console.log('timesChannelID:' + items.timesChannelID)
+      console.log('myName:' + items.myName)
+      console.log('toggl:' + items.toggl)
 
-    chrome.storage.sync.set({
-      token: token.value ? token.value : items.token,
-      channelID: channelID.value ? channelID.value : items.channelID,
-      myName: myName.value ? myName.value : items.myName
-    })
-  })
+      chrome.storage.sync.set({
+        token: token.value ? token.value : items.token,
+        channelID: channelID.value ? channelID.value : items.channelID,
+        timesChannelID: timesChannelID.value
+          ? timesChannelID.value
+          : items.timesChannelID,
+        myName: myName.value ? myName.value : items.myName,
+        toggl: toggl.value ? toggl.value : items.toggl
+      })
+    }
+  )
 
   chrome.storage.onChanged.addListener((changes, namespace) => {
     for (const [key, { oldValue, newValue }] of Object.entries(changes)) {
