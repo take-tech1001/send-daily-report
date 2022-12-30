b689f133456e16fb1c46fbf9d4dcc0710fc01ecf
nado1001 <tk.tech1001@gmail.com>
2022-12-31 02:34:10 +0900

    chore: remove

--- src/utils/consts.ts
+++
@@ -1,3 +0,0 @@
-export const FILES_UPLOAD_API_URL = 'https://slack.com/api/files.upload'
-export const POST_MESSAGE_API_URL = 'https://slack.com/api/chat.postMessage'
-export const TOGGL_API_URL = 'https://api.track.toggl.com/api/v8/'
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mode: 'jit',
-  content: ['./index.html', './src/**/*.{ts,html}'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
-}
