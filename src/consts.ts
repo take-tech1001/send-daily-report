87d8278a7a858ba223f72bc2cf5508b83ece8e6a
nado1001 <tk.tech1001@gmail.com>
2023-03-22 00:45:53 +0900

    chore: add time designer url

--- src/consts.ts
+++ src/consts.ts
@@ -4,6 +4,8 @@ export const POST_MESSAGE_API_URL = 'https://slack.com/api/chat.postMessage'
 export const CHANGE_STATUS_API_URL = 'https://slack.com/api/users.profile.set'
 export const TOGGL_API_URL = 'https://api.track.toggl.com/api/v8/'
 
+export const TIME_DESIGNER_API_URL = 'https://api.timedesigner.com/v1/'
+
 export const DIRECTOR_REPORT_NAMES = [
   'todayTasks', //【今日の対応内容】
   'issuesFound', //【見つかった課題（良い悪い困ったこと等振り返り）】
